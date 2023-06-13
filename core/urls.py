b6c84e2ea374fbf86b8041d11bfae4c56a2ccde8
Anurag Dev <kinganuragdev@gmail.com>
2023-06-13 20:31:40 +0530

    Initial Commit

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('rooms/', views.rooms, name='rooms'),
+]
---
+++ core/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+from django.contrib.auth import login, logout
+
+# Create your views here.
+def index(request):
+    return render(request, 'core/index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+
+    else:
+        form = SignUpForm()
+
+    return render(request, 'core/signup.html', {'form':form})
+
+def rooms(request):
+    return render(request, 'core/rooms.html')
